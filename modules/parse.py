95a61e90dc94d36c2d6738e24fd91849c5f9dcc1
r128 <111167388+r128w@users.noreply.github.com>
2023-08-26 15:45:53 +0000

    Update parse.py

--- modules/parse.py
+++ modules/parse.py
@@ -20,6 +20,30 @@ def groupParens(tokens):# takes in tokens, outputs tokens nested via b parens
         i+=1
     return output
 
-# def parseTokens(tokens):# takes in tokens, outputs instructions
-#     # step one: search through for "b" tokens, and group up lists so that
-
+# instructions in a list are formatted so each operation is its own separate sublist, and bbrackets are strings:
+# [["if", ["1", ">", "0"]], "bB", ["print", "\"bogos binted\""], "Bb"]
+def parseTokens(tokens):# takes in raw tokens, outputs instructions
+    # step one: search through for "b" tokens, and group up lists so that
+    tokens = groupParens(tokens)
+    output = []
+    i = 0
+    while i < len(tokens):
+        match(tokens[i]):
+            case "brint":
+                output.append(["print", tokens[i + 1]])
+                i+=2
+                continue
+            case "bif":
+                output.append(["if", tokens[i + 1]])
+                i+=2
+                continue
+            case "bB":
+                output.append("bB")
+                i+=1
+                continue
+            case "Bb":
+                output.append("Bb")
+                i+=1
+                continue
+        i+=1
+    return output
