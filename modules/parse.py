08432377c30d1bcca0d0e8f749c7434643507904
r128 <111167388+r128w@users.noreply.github.com>
2023-08-27 05:48:08 +0000

    Update parse.py

--- modules/parse.py
+++ modules/parse.py
@@ -20,8 +20,55 @@ def groupParens(tokens):# takes in tokens, outputs tokens nested via b parens
         i+=1
     return output
 
+def CACBTN(SIQ): # Check And Convert Bs To Number (String in Question)
+# if string is not perfectly formatted as bumber, it will just return the string unchanged
+    if SIQ[0:2] != "BB":
+        return SIQ
+    else:
+        counter = 0
+        for char in SIQ[2:]:
+            if char == "b":
+                counter+=1
+            else:
+                return SIQ
+        return counter
+
+def parseInBrackets(LIQ):# List in Question
+    # parser used on a set of brackets in code: ["bum", "bore", "BBb"] - > ["bum", ">", 1]
+    if type(LIQ) != list:
+        return LIQ# this should only operate on lists
+    if len(LIQ) == 1:
+        return LIQ[0]# if it's a list with one item, just return the item outside of brackets. this way, statements like "brint b "bogos binted" B" and "brint "bogos binted"" are equivalent
+    for i in range(len(LIQ)):# convert all the bumbers to integers
+        LIQ[i] = CACBTN(LIQ[i])
+        match(LIQ[i]):
+            case "blus":
+                LIQ = "+"
+            case "binus":
+                LIQ = "-"
+            case "bimes":
+                LIQ = "*"
+            case "bivide":
+                LIQ = "/"
+            case "bore":
+                LIQ = ">"
+            case "bess":
+                LIQ = "<"
+            case "biss":
+                LIQ = "="
+            case "band":
+                LIQ = "&"
+            case "binput":
+                LIQ = "input"
+            case "bot":
+                LIQ = "!"
+            case "bat":
+                LIQ = "@" 
+    return LIQ
+
+
 # instructions in a list are formatted so each operation is its own separate sublist, and bbrackets are strings:
-# [["if", ["1", ">", "0"]], "bB", ["print", "\"bogos binted\""], "Bb"]
+# [["if", [1, ">", 0]], "bB", ["print", "\"bogos binted\""], "Bb"]
 def parseTokens(tokens):# takes in raw tokens, outputs instructions
     # step one: search through for "b" tokens, and group up lists so that
     tokens = groupParens(tokens)
@@ -30,11 +77,15 @@ def parseTokens(tokens):# takes in raw tokens, outputs instructions
     while i < len(tokens):
         match(tokens[i]):
             case "brint":
-                output.append(["print", tokens[i + 1]])
+                output.append(["print", CACBTN(tokens[i + 1])])
                 i+=2
                 continue
             case "bif":
-                output.append(["if", tokens[i + 1]])
+                output.append(["if", CACBTN(tokens[i + 1])])
+                i+=2
+                continue
+            case "boop":
+                output.append(["while", CACBTN(tokens[i + 1])])
                 i+=2
                 continue
             case "bB":
@@ -44,6 +95,10 @@ def parseTokens(tokens):# takes in raw tokens, outputs instructions
             case "Bb":
                 output.append("Bb")
                 i+=1
+                continue
+            case "bis":
+                output.append(["setVar", CACBTN(tokens[i - 1]), CACBTN(tokens[i + 1])])
+                i+=2
                 continue
         i+=1
     return output
