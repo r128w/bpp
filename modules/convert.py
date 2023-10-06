4c4a2a2df7492715e7fcec285fb33feb257dce9b
r128 <111167388+r128w@users.noreply.github.com>
2023-10-06 05:29:47 +0000

    Update convert.py
    add string literal support

--- modules/convert.py
+++ modules/convert.py
@@ -1,5 +1,10 @@
 import sys
 
+def toggle(input):# flip true -> false, false -> true
+    if input == False:
+        return True
+    return False
+
 def convertFile(FIQ): # takes in a raw text file, and returns a list of strings (tokens) that were separated by spaces. all other whitespace is removed.
     file = open(FIQ, "r")
     lines = []
@@ -15,15 +20,34 @@ def convertFile(FIQ): # takes in a raw text file, and returns a list of strings 
     for i in range(emptyCount):
         lines.remove("")
     tokens = []
+
+    allLines = ""
     for line in lines:
-        lineTokens = line.split(" ")
-        for token in lineTokens:
-            tokens.append(token)# the token list here is every single sequence of characters separated by spaces - newlines ignored
+        allLines += " " + line
+    # tokens = allLines.split(" ")
+
+    tokenToAdd = ""
+    inStringLit = False
+    i = 0
+    while i < len(allLines):
+        if i > 1 and allLines[i] == "B":
+            if allLines[i-1] == "b" and allLines[i-2] == "B":
+                inStringLit = toggle(inStringLit)
+        if allLines[i] == " " and inStringLit == False:
+            tokens.append(tokenToAdd)
+            tokenToAdd = ""
+        else:
+            tokenToAdd+=allLines[i]
+        if i + 1 == len(allLines):# if at the end of the file
+            tokens.append(tokenToAdd)
+        i+=1
+
     # remove empty strings - meaning token list returned is every sequence of characters separated by whitespace, specifics ignored
-    emptyCount=0
+    emptyCount = 0
     for token in tokens:# just empty line removal function from above
         if token == "":
             emptyCount+=1
     for i in range(emptyCount):
         tokens.remove("")
+    print(tokens)
     return tokens
