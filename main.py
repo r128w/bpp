0ac8e056d1c2b3904d418f782e09f1de147d3ae4
r128 <111167388+r128w@users.noreply.github.com>
2023-11-07 06:05:21 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import modules.convert
 import modules.parse
 import modules.execute
+import sys
 
-modules.execute.executeInstructions(modules.parse.parseTokens(modules.convert.convertFile(input("File ? "))))
+ # run this like py main.py test.bpp or python3 main.py test.bpp (depending on method of running python)
+modules.execute.executeInstructions(modules.parse.parseTokens(modules.convert.convertFile(sys.argv[1])))
