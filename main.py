8154ee2f10c2ae5f9d545c9664a2a5fc04642f4f
r128 <111167388+r128w@users.noreply.github.com>
2023-11-16 12:28:12 +0000

    Update main.py
    clarify explanation in code (though fairly self-explanatory)

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import modules.parse
 import modules.execute
 import sys
 
- # run this like py main.py test.bpp or python3 main.py test.bpp (depending on method of running python)
+ # run this like "py main.py test.bpp" or "python3 main.py test.bpp" (depending on method of running python)
 modules.execute.executeInstructions(modules.parse.parseTokens(modules.convert.convertFile(sys.argv[1])))
